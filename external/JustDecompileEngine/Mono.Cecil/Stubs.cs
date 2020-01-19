8c82740a54fc3a16750eb2ad591d66f2d991b2bb
Jonathan Pobst <monkey@jpobst.com>
2020-01-18 20:00:50 -0600

    Pull in JustDecompileEngine and update to netstandard2.0.

---
+++ external/JustDecompileEngine/Mono.Cecil/Stubs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mono.Cecil
+{
+    class Registry
+    {
+        internal static LocalMachine LocalMachine;
+
+        public static string GetValue (string hive, string key, object def) => null;
+    }
+
+    class LocalMachine
+    {
+        public string Name { get; set; }
+    }
+}
